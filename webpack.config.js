8cfd8b42657fdd2a6c56095d03d810a780cb346c
NancyBolstad <litchacha@gmail.com>
2019-11-17 14:01:55 +0100

    Added bootstrap

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,15 @@ module.exports = {
           'css-loader',
           // Compiles Sass to CSS
           'sass-loader',
+          {
+            loader: 'postcss-loader', // Run post css actions
+            options: {
+              plugins: function() {
+                // post css plugins, can be exported to postcss.config.js
+                return [require('precss'), require('autoprefixer')];
+              },
+            },
+          },
         ],
       },
     ],
