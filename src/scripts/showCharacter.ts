d93115e3a466734f692b1bf0993de82ca6a0dc2b
NancyBolstad <litchacha@gmail.com>
2019-11-30 20:25:17 +0100

    wip

--- src/scripts/showCharacter.ts
+++ src/scripts/showCharacter.ts
@@ -11,7 +11,7 @@ async function showCharacter(characterName: number) {
     const data: ResponseObjTypes = await response.json();
 
     const component = document.createElement('div');
-    component.className = 'block--select__card';
+    component.className = 'col-lg-4 p-3 col-md-6';
     component.innerHTML = ` <div class="p-4"><h4>${data.name}</h4><p lass="package__description">${data.born}</p></div>`;
 
     if (characterList != null) characterList.append(component);
