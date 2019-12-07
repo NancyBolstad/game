c6057da8a39da6c8ac67de26c8fc589ec579eb6c
NancyBolstad <litchacha@gmail.com>
2019-12-07 19:19:35 +0100

    Refactor game logic

--- src/playGame.ts
+++ src/playGame.ts
@@ -43,6 +43,7 @@ function playGame(player1: number, player2: number): void {
     player1Status += currentDicePoint;
 
     if (player1Status >= 30) {
+      removePlayer(player1);
       const finalPosition = document.getElementById('tile-index-30');
       displayPlayers(finalPosition, player1);
       player1Btn.disabled = true;
@@ -50,121 +51,74 @@ function playGame(player1: number, player2: number): void {
       console.log('Player 1 winner');
       alert('Winner! Player 1 got the throne!');
       return null;
-    }
-    console.log(`Player 1 rolled: ${currentDicePoint}`);
-
-    const updatePosition = document.getElementById(`tile-index-${player1Status}`);
-    removePlayer(player1);
-    displayPlayers(updatePosition, player1);
-
-    if (player1Status != updatePlayer1Status()) {
-      removePlayer(player1);
-      const index = updatePlayer1Status();
-      const trapPosition = document.getElementById(`tile-index-${index}`);
-      displayPlayers(trapPosition, player1);
-    }
-
-    if (currentDicePoint === 6) {
-      console.log('Since you rolled 6, you got a Bonus movement. Roll the dice again');
     } else {
-      console.log(`player1Status:${player1Status}`);
-      console.log(`player2Status:${player2Status}`);
-      isPlayer1Turn = false;
-      updateButton(isPlayer1Turn);
-    }
-  }
+      console.log(`Player 1 rolled: ${currentDicePoint}`);
 
-  function updatePlayer1Status(): number {
-    switch (player1Status) {
-      case traps[0]:
-        return player1Status - 1;
-        console.log(`Trap 1: move back 1 steps >>>Current player 1: ${player1Status}`);
-        break;
-      case traps[1]:
-        return player1Status - 2;
-        console.log(`Trap 2: move back 2 steps >>>Current player 1: ${player1Status}`);
-        break;
-      case traps[2]:
-        return player1Status - 3;
-        console.log(`Trap 3: move back 3 steps >>>Current player 1: ${player1Status}`);
-        break;
-      case traps[3]:
-        return player1Status - 4;
-        console.log(`Trap 4: move back 4 steps >>>Current player 1: ${player1Status}`);
-        break;
-      case traps[4]:
-        return player1Status - 5;
-        console.log(`Trap 5: move back 5 steps >>>Current player 1: ${player1Status}`);
-        break;
-      default:
-        return 0;
-        console.log(`Normal: >>>Current player 1: ${player1Status}`);
-        break;
+      const updatePosition = document.getElementById(`tile-index-${player1Status}`);
+      removePlayer(player1);
+      displayPlayers(updatePosition, player1);
+
+      if (traps.includes(player1Status)) {
+        player1Status -= traps.indexOf(player1Status) + 1;
+        const newPosition: HTMLElement = document.getElementById(`tile-index-${player1Status}`);
+        alert(`Trap ${traps.indexOf(player1Status) + 1}`);
+
+        setTimeout(function() {
+          removePlayer(player1);
+          displayPlayers(newPosition, player1);
+        }, 1000); //delay is in milliseconds
+      }
+
+      if (currentDicePoint === 6) {
+        alert('Since you rolled 6, you got a Bonus movement. Roll the dice again');
+      } else {
+        console.log(`player1Status:${player1Status}`);
+        console.log(`player2Status:${player2Status}`);
+        isPlayer1Turn = false;
+        updateButton(isPlayer1Turn);
+      }
     }
   }
 
   function runPlayer2Turn(): void {
     const currentDicePoint = rollDice();
     showDiceResult(diceContainer, currentDicePoint);
-    console.log(`Player 2 rolled: ${currentDicePoint}`);
-    removePlayer(player2);
-    player1Status += currentDicePoint;
+    player2Status += currentDicePoint;
 
     if (player2Status >= 30) {
+      removePlayer(player2);
       const finalPosition = document.getElementById('tile-index-30');
       displayPlayers(finalPosition, player2);
       player1Btn.disabled = true;
       player2Btn.disabled = true;
       console.log('Player 2 winner');
-      alert('Winner!!!');
-      return null;
-      return null;
-    }
-
-    const updatePosition = document.getElementById(`tile-index-${player1Status}`);
-    displayPlayers(updatePosition, player2);
-
-    if (currentDicePoint === 6) {
-      updatePlayer2Status();
-      console.log('Since you rolled 6, you got a Bonus movement');
-      return null;
+      alert('Winner! Player 2 got the throne!');
     } else {
-      updatePlayer2Status();
-    }
-    console.log(`player1Status:${player1Status}`);
-    console.log(`player2Status:${player2Status}`);
-    isPlayer1Turn = true;
-    updateButton(isPlayer1Turn);
-  }
-  function updatePlayer2Status() {
-    switch (player2Status) {
-      case traps[0]:
-        player2Status -= 1;
-        console.log(`Trap 1: move back 1 steps >>>Current player 2: ${player2Status}`);
-        break;
-      case traps[1]:
-        player2Status -= 2;
-        console.log(`Trap 2: move back 2 steps >>>Current player 2: ${player2Status}`);
-        break;
-      case traps[2]:
-        player2Status -= 3;
-        console.log(`Trap 3: move back 3 steps >>>Current player 2: ${player2Status}`);
-        break;
-      case traps[3]:
-        player2Status -= 4;
-        console.log(`Trap 4: move back 4 steps >>>Current player 2: ${player2Status}`);
-        break;
-      case traps[4]:
-        player2Status -= 5;
-        console.log(`Trap 5: move back 5 steps >>>Current player 2: ${player2Status}`);
-        break;
-      case traps[5]:
-        player2Status -= 6;
-        console.log(`Trap 6: move back 6 steps >>>Current player 2: ${player2Status}`);
-        break;
-      default:
-        console.log(`Normal: >>>Current player 2: ${player2Status}`);
-        break;
+      alert(`Player 2 rolled: ${currentDicePoint}`);
+
+      const updatePosition = document.getElementById(`tile-index-${player2Status}`);
+      removePlayer(player2);
+      displayPlayers(updatePosition, player2);
+
+      if (traps.includes(player2Status)) {
+        player2Status -= traps.indexOf(player2Status) + 1;
+        const newPosition: HTMLElement = document.getElementById(`tile-index-${player2Status}`);
+        alert(`Trap ${traps.indexOf(player2Status) + 1}`);
+
+        setTimeout(function() {
+          removePlayer(player2);
+          displayPlayers(newPosition, player2);
+        }, 1000); //delay is in milliseconds
+      }
+
+      if (currentDicePoint === 6) {
+        alert('Since you rolled 6, you got a Bonus movement. Roll the dice again');
+      } else {
+        console.log(`player1Status:${player1Status}`);
+        console.log(`player2Status:${player2Status}`);
+        isPlayer1Turn = true;
+        updateButton(isPlayer1Turn);
+      }
     }
   }
 }
--- src/scripts/util/traps.ts
+++ src/scripts/util/traps.ts
@@ -1,1 +1,1 @@
-export const traps: number[] = [8, 14, 20, 24, 28];
+export const traps: number[] = [8, 14, 18, 24, 28];
