27d7e1f24439c739b1b1822a816ff128b7782443
NancyBolstad <litchacha@gmail.com>
2019-12-08 19:56:43 +0100

    Added more characters to select

--- src/scripts/util/characterIndex.ts
+++ src/scripts/util/characterIndex.ts
@@ -4,5 +4,18 @@ const cersei: number = 238;
 const tyrion: number = 1052;
 const daenerys: number = 271;
 
-const characterIndex: number[] = [samwell, jon, cersei, tyrion, daenerys];
+const characterIndex: number[] = [
+  samwell,
+  jon,
+  cersei,
+  tyrion,
+  daenerys,
+  samwell,
+  jon,
+  cersei,
+  tyrion,
+  daenerys,
+  tyrion,
+  daenerys,
+];
 export default characterIndex;
